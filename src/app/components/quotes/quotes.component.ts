65ba6fbb53f82ea4de6bb381ffb35c99c7bd33da
Mantongash <you@example.com>
2019-11-30 10:57:24 +0300

    Imported the Quote class and added some sample quotes

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Quote } from "../quote";
 
 @Component({
-  selector: 'app-quotes',
-  templateUrl: './quotes.component.html',
-  styleUrls: ['./quotes.component.css']
+  selector: "app-quotes",
+  templateUrl: "./quotes.component.html",
+  styleUrls: ["./quotes.component.css"]
 })
 export class QuotesComponent implements OnInit {
+  quotes: Quote[] = [
+    {
+      name: "Anthony Ng'ang'a",
+      actualQuote:
+        "There is nothing more powerful in the world than the idea that came in time.",
+      author: "Victor Hugo"
+    },
+    {
+      name: "James O'brien",
+      actualQuote:
+        "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
+      author: "Aristotle"
+    },
+    {
+      name: "John Doe",
+      actualQuote:
+        "Never bend your head. Always hold it high. Look the world straight in the eye.",
+      author: "Hellen Keller"
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

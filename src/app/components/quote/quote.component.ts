4f45945c3ec962541c055ee2bea64407213c14d1
Mantongash <you@example.com>
2019-12-01 10:32:58 +0300

    Refactored/ restructured my code and components

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 import { Quote } from "../quote";
 
 @Component({
@@ -7,27 +7,8 @@ import { Quote } from "../quote";
   styleUrls: ["./quote.component.css"]
 })
 export class QuoteComponent implements OnInit {
-  quotes: Quote[] = [
-    {
-      name: "Anthony Ng'ang'a",
-      actualQuote:
-        "There is nothing more powerful in the world than the idea that came in time.",
-      author: "Victor Hugo"
-    },
-    {
-      name: "James O'brien",
-      actualQuote:
-        "We are what we repeatedly do. Excellence, then, is not an act, but a habit.",
-      author: "Aristotle"
-    },
-    {
-      name: "John Doe",
-      actualQuote:
-        "Never bend your head. Always hold it high. Look the world straight in the eye.",
-      author: "Hellen Keller"
-    }
-  ];
 
+@Input() quote: Quote;
   constructor() {}
 
   ngOnInit() {}
